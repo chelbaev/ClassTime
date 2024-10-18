f50055b2009fce251423b17225017ffe3839b1d4
Челбаев Михаил Сергеевич <146612605+chelbaev@users.noreply.github.com>
2024-10-18 21:42:36 +0700

    Update time.cpp

--- TimeLib/time.cpp
+++ TimeLib/time.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include "time.hpp"
 
-Time::Time(bool flag):flag(flag){
+Time::Time(bool flag):hours(0), minutes(0), seconds(0), flag(flag){
     if (flag){
         count += 1;
         std::cout << "I create new time! current count = " << count << "\n";
@@ -13,40 +13,46 @@ Time::Time(int h, int m, int s, bool flag): hours(h), minutes(m), seconds(s), fl
         count += 1;
         std::cout << "I create new time! current count = " << count << "\n";
     }
+    if (this->hours > 23 || this->hours < 0 || this->minutes > 59 || this->minutes < 0 || this->seconds > 59 || this->seconds < 0 )
+        this->Normalize();
 }
 Time::~Time(){
     if (this->flag){
         count -= 1;
-        std::cout << "I delete time! current count = " << count << "\n";
+        std::cout << "I delete time! current count = " << count << " " << *this << "\n";
     }
 }
 Time::Time(const Time& t): Time(t.hours, t.minutes, t.seconds, true) {}
-
 
 void Time::PrintTime(){
     std::cout << hours << ":" \
     << minutes << ":" << seconds << std::endl;
 }
 
-void Time::SetHours(int hours){this->hours = hours; };
-void Time::SetMinutes(int minutes){this->minutes = minutes; };
-void Time::SetSeconds(int seconds){this->seconds = seconds; };
+void Time::SetHours(int hours){this->hours = hours; this->Normalize();};
+void Time::SetMinutes(int minutes){this->minutes = minutes; this->Normalize(); };
+void Time::SetSeconds(int seconds){this->seconds = seconds; this->Normalize(); };
 
 int Time::GetHours() const { return hours; }
 int Time::GetMinutes() const { return minutes; }
 int Time::GetSeconds() const { return seconds; }
 
-int Time::ToSeconds(){ return (24 * 3600 + this->GetHours() * 3600 + this->GetMinutes() * 60 + this->GetSeconds()) % (24 * 3600); }
+int Time::ToSeconds() const{ return (24 * 3600 + this->GetHours() * 3600 + this->GetMinutes() * 60 + this->GetSeconds()) % (24 * 3600); }
 Time Time::Normalize () { 
+    if (this->hours > 23 || this->hours < 0 ||\
+        this->minutes > 59 || this->minutes < 0 || \
+        this->seconds > 59 || this->seconds < 0 ){    
     int seconds = this->ToSeconds();
-    return Time(this->hours = seconds / 3600, this->minutes = (seconds % 3600) / 60, this->seconds = seconds%60); 
+    return Time(this->hours = seconds / 3600, this->minutes = (seconds % 3600) / 60, this->seconds = seconds%60, false); 
+    }
+    return Time(this->hours, this->minutes, this->seconds,false);
 }
 
 Time& Time::operator= (const Time& other){
     this->hours = other.hours; 
     this->minutes = other.minutes; // для примера
     this->seconds = other.seconds;
-    this->flag = true;
+    this->flag = other.flag;
     return *this;
 }
 Time& Time::operator += (int s) {
@@ -59,17 +65,36 @@ Time& Time::operator -= (int s) {
     this->Normalize();
     return *this;
 }
-int Time::operator == (const Time other){
+Time& Time::operator += (const Time& a){
+    *this += a.ToSeconds();
+    return *this;
+}
+Time& Time::operator -= (const Time& a){
+    *this -= a.ToSeconds();
+    return *this;
+
+}
+bool Time::operator == (const Time& other){
     if (this->seconds == other.seconds && this->minutes == other.minutes && this->hours == other.hours)
-        return 1;
-    return 0;
+        return true;
+    return false;
 }
 
 Time operator+ (const Time& t, int s) {
-   return Time(t.GetHours(), t.GetMinutes(), t.GetSeconds() + s).Normalize();
+   return Time(t.GetHours(), t.GetMinutes(), t.GetSeconds() + s, false).Normalize();
 }
 Time operator- (const Time& t, int s) {
-   return Time(t.GetHours(), t.GetMinutes(), t.GetSeconds() - s).Normalize();
+   return Time(t.GetHours(), t.GetMinutes(), t.GetSeconds() - s, false).Normalize();
+}
+
+std::ostream& operator<<(std::ostream& os, const Time& time){
+    os  << time.GetHours() << ":"\
+        << time.GetMinutes() << ":"\
+        << time.GetSeconds();
+    return os;
+}
+std::string TimeNameSpace::to_string(const Time& a) {
+    return std::to_string(a.GetHours()) + ":" + std::to_string(a.GetMinutes()) + ":" + std::to_string(a.GetSeconds());
 }
 
 unsigned Time::count = 0 ;
